e39d0cb47287d977bb0dfee2f23b3aa73abae6ca
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2015-05-05 00:01:36 +0200

    dissapear header on top

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,43 @@ $(function(){
     $.scrollTo('#stop',800);
     return false;
   });
+
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var heightWithoutHeader = 300;
+  var navbarHeight = $('header').outerHeight();
+
+  // on scroll, let the interval function know the user has scrolled
+  $(window).scroll(function(event){
+    didScroll = true;
+  });
+  // run hasScrolled() and reset didScroll status
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+    if (Math.abs(lastScrollTop - st) <= delta)
+      return;
+
+    // If current position > last position AND scrolled past navbar...
+    if (st > lastScrollTop && st > navbarHeight){
+      // Scroll Down
+      $('header').removeClass('nav-up');
+    } else {
+      // Scroll Up
+      // If did not scroll past the document (possible on mac)...
+      if(st + $(window).height() - heightWithoutHeader < $(document).height()) { 
+        $('header').addClass('nav-up');
+      }
+    }
+
+    lastScrollTop = st;
+
+  }
 });
