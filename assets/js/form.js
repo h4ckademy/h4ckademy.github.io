cbad7633e127ecfcbd7c114dea65f2b8d19c7a40
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2015-09-16 10:42:24 +0200

    add gender and job form fields

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('#contact_form input, #contact_form textarea, #contact_form select').on('focusin', function() {
+  $('#contact_form input[type!=checkbox], #contact_form textarea, #contact_form select').on('focusin', function() {
     $(this).parent().addClass('input--filled');
   })
   .on('focusout', function() {
